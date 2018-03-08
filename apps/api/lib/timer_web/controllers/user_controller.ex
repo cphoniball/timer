5d63ebbdb5ad1da6b096ca966df52cb415f08b7c
Chris Honiball <choniball@atiba.com>
2018-03-08 12:41:35 -0600

    Add todo about fallback controller

--- apps/api/lib/timer_web/controllers/user_controller.ex
+++ apps/api/lib/timer_web/controllers/user_controller.ex
@@ -4,6 +4,8 @@ defmodule TimerWeb.UserController do
   alias Timer.{Repo, User}
   alias TimerWeb.ApiView
 
+  # TODO: Use fallback controller to clean up duplicated logic here
+
   def index(conn, _params) do
     users = Repo.all(User)
     render(conn, "index.json", %{users: users})
