5edf27c842c5ee32457e2a6766c9160420ab4e9f
Chris Honiball <choniball@atiba.com>
2018-03-15 18:02:45 -0500

    Simplify time entry controller test

--- apps/api/test/timer_web/controllers/time_entry_controller_test.exs
+++ apps/api/test/timer_web/controllers/time_entry_controller_test.exs
@@ -4,15 +4,17 @@ defmodule TimerWeb.TimeEntryControllerTest do
   @description "This is a test description for a time entry."
 
   setup _context do
+    user = insert(:user)
+
     %{
       conn: build_conn(),
-      user: insert(:user)
+      body: %{"time_entry" => %{"user" => %{"id" => user.id}}}
     }
   end
 
   describe "start" do
-    test "should create a time entry with the current time", %{conn: conn, user: user} do
-      with conn <- post(conn, time_entry_path(conn, :start, %{"time_entry" => %{"user" => %{"id" => user.id}}})),
+    test "should create a time entry with the current time", %{conn: conn, body: body} do
+      with conn <- post(conn, time_entry_path(conn, :start, body)),
            response <- json_response(conn, 201),
            {:ok, started_at} <- DateTime.from_iso8601(response["data"]["started_at"])
       do
@@ -20,8 +22,9 @@ defmodule TimerWeb.TimeEntryControllerTest do
       end
     end
 
-    test "should create a time entry with the given description", %{conn: conn, user: user} do
-      with conn <- post(conn, time_entry_path(conn, :start, %{"time_entry" => %{"description" => @description, "user" => %{"id" => user.id}}})),
+    test "should create a time entry with the given description", %{conn: conn, body: body} do
+      with body_with_description = put_in(body["time_entry"]["description"], @description),
+           conn <- post(conn, time_entry_path(conn, :start, body_with_description)),
            response <- json_response(conn, 201)
       do
         assert response["data"]["description"] == @description
