36c952643c6bda36225ae8d3e998cb06af52a921
Chris Honiball <choniball@atiba.com>
2018-08-04 12:42:35 -0500

    Style client list

--- apps/client/src/clients/Clients.tsx
+++ apps/client/src/clients/Clients.tsx
@@ -11,8 +11,29 @@ interface Props {
     onCreateClient(client: Client): void;
 }
 
+const CreateClientContainer = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: row-reverse;
+`;
+
 const StyledCreateClient = styled<CreateClientProps>(CreateClient)`
     float: right;
+`;
+
+const ClientList = styled.ul`
+    display: block;
+    width: 100%;
+    list-style: none;
+`;
+
+const ClientListItem = styled.li`
+    width: 100%;
+    font-size: ${props => props.theme.font.base.size};
+    margin-bottom: 15px;
+    padding: 10px 12px;
+    background-color: ${props => props.theme.color.white};
+    box-shadow: 0px 1px 2px ${props => props.theme.color.main}88;
 `;
 
 const Clients: React.StatelessComponent<Props> = ({ clients, isFetching, onCreateClient }) => {
@@ -20,15 +41,17 @@ const Clients: React.StatelessComponent<Props> = ({ clients, isFetching, onCreat
 
     return (
         <div>
-            <StyledCreateClient onSubmit={onCreateClient} />
+            <CreateClientContainer>
+                <CreateClient onSubmit={onCreateClient} />
+            </CreateClientContainer>
             {!clients.length && <span>You haven't created any clients yet.</span>}
-            {!!clients.length && <ul>
+            {!!clients.length && <ClientList>
                 {clients.map(client => {
                     return (
-                        <li className="client" key={`client-${client.id}`}>{client.name}</li>
+                        <ClientListItem className="client" key={`client-${client.id}`}>{client.name}</ClientListItem>
                     );
                 })}
-            </ul>}
+            </ClientList>}
         </div>
     );
 };
