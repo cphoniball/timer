70c281eda72b9be49f08a45dc5e0f6bc754b0ccc
Chris Honiball <choniball@atiba.com>
2018-09-12 05:21:49 -0500

    Add styling to past time entries

--- apps/client/src/timer/FinishedTimeEntries.jsx
+++ apps/client/src/timer/FinishedTimeEntries.jsx
@@ -1,20 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 import moment from 'moment';
 
+// TODO: These are the same as the client conatiners, we can reuse these styles
+const TimeEntryList = styled.ul`
+    display: block;
+    width: 100%;
+    list-style: none;
+    margin-top: 30px;
+`;
+
+const TimeEntryListItem = styled.li`
+    width: 100%;
+    font-size: ${props => props.theme.font.base.size};
+    margin-bottom: 15px;
+    padding: 10px 12px;
+    background-color: ${props => props.theme.color.white};
+    box-shadow: 0px 1px 2px ${props => props.theme.color.main}88;
+`;
+
 const TimeEntry = ({ timeEntry }) => (
-    <li>
+    <TimeEntryListItem>
         <span>{timeEntry.description}</span>
         <span>Started: {timeEntry.started_at}</span>
         <span>Ended: {timeEntry.ended_at}</span>
-    </li>
+    </TimeEntryListItem>
 );
 
 const FinishedTimeEntries = ({ timeEntries }) => (
-    <ul>
+    <TimeEntryList>
         {timeEntries.map(timeEntry => <TimeEntry timeEntry={timeEntry} />)}
-    </ul>
+    </TimeEntryList>
 );
 
 FinishedTimeEntries.propTypes = {
